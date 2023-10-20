74f6f5c359947c72ca373db1d9641f7c66fd0c9b
soul <soulaiman.soulb@gmail.com>
2023-10-21 00:21:41 +0100

    file

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+
+export default function Header() {
+
+  const navigate = useNavigate();
+  
+ 
+  return (
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
+          </h1>
+        </Link>
+        <form
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+          />
+          <button>
+          </button>
+        </form>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
+          </Link>
+        
+        </ul>
+      </div>
+    </header>
+  );
+}
