cfe00c272c3b5d8157178fb821ffa761a6704595
soul <soulaiman.soulb@gmail.com>
2023-10-23 00:17:46 +0100

    redux fixed

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,42 +1,46 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+
 export default function SignIn() {
-    const [formData,setFormData] =useState({})
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
-   const  handleChange=(e)=>{
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
     setFormData({
-        ...formData,[e.target.id]:e.target.value,
-    })
-    
-   }
-    const handleSubmit=async(e)=>{
-        e.preventDefault()
-        try {
-            setLoading(true);
-            const res =await fetch("/api/auth/signin",{
-                method:"POST",
-                headers:{
-                    'Content-Type': 'application/json',
-                },
-                body:JSON.stringify(formData),
-            });
-            const data = await res.json();
-            console.log(data);
-            if (data.success === false) {
-                setLoading(false);
-                setError(data.message);
-                return;
-              }
-            setLoading(false);
-            setError(null);
-            navigate('/');
-        } catch (error) {
-            setLoading(false);
-            setError(error.message);
-        }
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
@@ -62,7 +66,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
-
+  
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
