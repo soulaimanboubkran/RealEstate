adf29434a9232f6db5225d7e758adfb0aea6fd27
soul <soulaiman.soulb@gmail.com>
2023-10-23 21:56:41 +0100

    redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -11,7 +11,7 @@ const persistConfig = {
   version: 1,
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const   persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
