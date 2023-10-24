f2b2c40655ae4ec5963f8996572a979aee6a73d5
soul <soulaiman.soulb@gmail.com>
2023-10-24 21:14:47 +0100

    profile ui

--- client/src/components/PriveteRoute.jsx
+++ client/src/components/PriveteRoute.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
 
-export default function PriveteRoute() {
-  return (
-    <div>PriveteRoute</div>
-  )
+export default function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
 }
