253b368ad071d5f3595e6b9f71638523a79fcf47
soul <soulaiman.soulb@gmail.com>
2023-10-24 20:45:25 +0100

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
+
+  server: { 
+    define: {
+    'import.meta.env.VITE_FIREBASE_API_KEY': process.env.FIREBASE,
+  },
+
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
@@ -11,6 +16,6 @@ export default defineConfig({
       },
     },
   },
-
+ 
   plugins: [react()],
 });
