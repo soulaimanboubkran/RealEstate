466dacb7c0823f74878dbc6add4f9da39d28d6c1
soul <soulaiman.soulb@gmail.com>
2023-10-27 23:51:19 +0100

    commit

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,8 +27,7 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
-  const [showListingsError, setShowListingsError] = useState(false);
-  const [userListings, setUserListings] = useState([]);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
