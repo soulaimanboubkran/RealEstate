da5d5e5eee4e9af1dfdea4f201fd0f4d910582e3
soul <soulaiman.soulb@gmail.com>
2023-10-29 19:39:55 +0100

    listing api create

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,7 +1,13 @@
+import Listing from "../models/listing.module.js"
 
 
 
 
 export const createListing = async(req,res,next)=>{
-
+    try {
+        const listing = Listing.create(req.body);
+        return res.status(201).json(listing);
+    } catch (error) {
+        next(error)
+    }
 }
--- api/routes/linsting.rout.js
+++ api/routes/linsting.rout.js
@@ -1,11 +1,12 @@
 import express  from "express";
 import { createListing } from "../controllers/listing.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 
 
 
-router.post('/create',createListing)
+router.post('/create',verifyToken ,createListing)
 
 
 
