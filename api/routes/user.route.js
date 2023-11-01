a228329de06b945ca5197c59b830062c4baa5f15
soul <soulaiman.soulb@gmail.com>
2023-11-01 23:12:02 +0100

    get listings client

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getUserListings, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 
@@ -7,5 +7,5 @@ const router = express.Router();
 router.get("/test",test);
 router.post("/update/:id",verifyToken,updateUser);
 router.delete("/delete/:id",verifyToken,deleteUser);
-
+router.get("/listings/:id",verifyToken,getUserListings);
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,7 +29,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
-
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
 
@@ -134,6 +135,21 @@ const handleSignOut=async()=>{
     dispatch(signOutUserFailure(data.message))
   }
 }
+
+
+const handleShowListings =async()=>{
+  try {
+    const res= await fetch(`/api/user/listings/${currentUser._id}`);
+    const data  = await res.json();
+    if(data.success === false){
+      setShowListingsError(true);
+      return;
+    }
+    setUserListings(data);
+  } catch (error) {
+    setShowListingsError(true);
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -208,6 +224,44 @@ const handleSignOut=async()=>{
       <p className='text-green-700 mt-5 font-bold'>
         {updateSuccess ? `User is updated successfully! `: ''}
       </p>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>
+        Show Listings
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+      {userListings && userListings.length > 0 && 
+        userListings.map((listing)=>(
+          <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+             <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+          </div>
+        ))
+
+      }
     </div>
   )
 }
