5f2acd22f83c973446eb35b84b3501844f4ff8cf
soul <soulaiman.soulb@gmail.com>
2023-11-02 00:17:14 +0100

    delete listing client

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -150,6 +150,26 @@ const handleShowListings =async()=>{
     setShowListingsError(true);
   }
 }
+
+
+const handleListingDelete = async(listingId)=>{
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      console.log(data.message);
+      return;
+    }
+
+    setUserListings((prev) =>
+      prev.filter((listing) => listing._id !== listingId)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -249,7 +269,7 @@ const handleShowListings =async()=>{
 
               <div className='flex flex-col item-center'>
                 <button
-                
+                  onClick={()=>handleListingDelete(listing._id)}
                   className='text-red-700 uppercase'
                 >
                   Delete
