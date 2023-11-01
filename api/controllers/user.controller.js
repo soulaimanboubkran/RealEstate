dc88572dafc757a82ca221575e3d8a58da9fc7b4
soul <soulaiman.soulb@gmail.com>
2023-11-01 11:52:36 +0100

    getUserLinstings api action ✨

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.module.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs';
@@ -51,3 +52,17 @@ export const deleteUser = async(req,res,next)=>{
         next(error)
       }
 }
+
+export const getUserListings = async(req,res,next)=>{
+   if(req.user.id !== req.params.id ){
+    try {
+        const listings = await  Listing.find({userRef:req.params.id});
+        res.status(200).json(listings)
+    } catch (error) {
+        next(error);
+
+    }
+   }else{
+    return next(errorHandler(401,'You can only view your own listings!'))
+   }
+};
