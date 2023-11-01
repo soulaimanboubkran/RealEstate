2f8291384bbbcfedb624cf82f930fd5bd82bad81
soul <soulaiman.soulb@gmail.com>
2023-11-01 23:45:53 +0100

    delete listing api action

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.module.js"
+import { errorHandler } from "../utils/error.js";
 
 
 
@@ -10,4 +11,20 @@ export const createListing = async(req,res,next)=>{
     } catch (error) {
         next(error)
     }
+}
+export const deleteLstings = async(req,res,next)=>{
+   const listing = await Listing.findById(req.params.id);
+if(!listing){
+    return next(errorHandler(404,'Listing not found1'));
+}
+if(req.user.id !== listing.userRef){
+    return next(errorHandler(401,'You can only delete your own listings!'));
+}
+try {
+
+    await Listing.findByIdAndDelete(req.params.id);
+res.status(200).json('listings has been deleleted!')
+} catch (error) {
+    next(error);
+}
 }
--- api/routes/linsting.rout.js
+++ api/routes/linsting.rout.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, deleteLstings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 
 router.post('/create',verifyToken ,createListing)
+router.delete('/delete/:id',verifyToken ,deleteLstings)
 
 
 
