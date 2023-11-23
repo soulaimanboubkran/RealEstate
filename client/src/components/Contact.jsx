69a5b0bd280c5bfde1da13b798727731692334dc
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-11-23 23:11:44 +0100

    commit

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -22,7 +22,31 @@ export default function Contact({ listing }) {
   }, [listing.userRef]);
   return (
     <>
-     
+      {landlord && (
+        <div className='flex flex-col gap-2'>
+          <p>
+            Contact <span className='font-semibold'>{landlord.username}</span>{' '}
+            for{' '}
+            <span className='font-semibold'>{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name='message'
+            id='message'
+            rows='2'
+            value={message}
+            onChange={onChange}
+            placeholder='Enter your message here...'
+            className='w-full border p-3 rounded-lg'
+          ></textarea>
+
+          <Link
+          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+          >
+            Send Message          
+          </Link>
+        </div>
+      )}
     </>
   );
 }
