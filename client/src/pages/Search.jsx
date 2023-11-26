cd14a76f86fadf9e019462484afedb10469aa512
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-11-27 00:10:34 +0100

    ShowMore

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,7 +15,7 @@ export default function Search() {
       });
       const [loading, setLoading] = useState(false);
       const [listings, setListings] = useState([]);
-      
+      const [showMore, setShowMore] = useState(false);
   const handleChange = (e) => {
     if (
       e.target.id === 'all' ||
@@ -84,6 +84,11 @@ export default function Search() {
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if (data.length > 8) {
+                setShowMore(true);
+              } else {
+                setShowMore(false);
+              }
             setListings(data);
             setLoading(false);
           };
@@ -103,6 +108,19 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+      };
+      const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
       };
     
       return (
@@ -226,7 +244,14 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
-
+  {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
           
         </div>
       </div>
